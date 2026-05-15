0b85c2936b1727c752c6667a30a5776d04f0b93e
Marcelo Ramirez <theg119.g@gmail.com>
2026-05-14 21:07:42 -0600

    Orden fifo de inventario y por lotes, modificados inventario_controller.py e inventario.html

--- controllers/inventario_controller.py
+++ controllers/inventario_controller.py
@@ -11,11 +11,19 @@ def inventario():
     conn = get_connection()
     cursor = conn.cursor(dictionary=True)
     cursor.execute("""
-        SELECT p.nombre,
-               SUM(l.cantidad_disponible) AS stock_total
+        SELECT 
+            p.nombre,
+            l.numero_lote,
+            l.fecha_ingreso,
+            l.fecha_vencimiento,
+            l.cantidad_disponible
         FROM productos p
-        JOIN lotes l ON p.id_producto = l.id_producto
-        GROUP BY p.id_producto
+        JOIN lotes l 
+            ON p.id_producto = l.id_producto
+        WHERE l.cantidad_disponible > 0
+        ORDER BY 
+            p.nombre ASC,
+            l.fecha_ingreso ASC
     """)
     datos = cursor.fetchall()
     conn.close()
